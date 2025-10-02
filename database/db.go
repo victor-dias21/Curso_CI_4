a6325b3bcd091e8d96a207bcddd0a61420218976
Victor Dias <victorrebelodias@gmail.com>
2025-10-02 16:10:49 -0300

    desativa ssl

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ var (
 
 func ConectaComBancoDeDados() {
 	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=require sslmode=verify-full sslrootcert=/home/ec2-user/rds-ca-2019-root.pem"
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
